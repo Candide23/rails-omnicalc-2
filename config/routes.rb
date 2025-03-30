77f2be2d5cd069a37b1de66fda2cf0143d2149d3
Candide Mboungou-Kimpolo <89795037+Candide23@users.noreply.github.com>
2025-03-30 23:21:54 +0000

    fixed more bugs

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   get("/wizard_subtract", { :controller => "subtraction", :action => "subtract_these" })
 
   get("/multiply", { :controller => "multiplication", :action => "show_multiplication_form" })
-  get("/wizardmultiply", { :controller => "multiplication", :action => "multiply_these" })
+  get("/wizard_multiply", { :controller => "multiplication", :action => "multiply_these" })
 
   get("/divide", { :controller => "division", :action => "show_division_form" })
   get("/wizard_divide", { :controller => "division", :action => "divide_these" })
